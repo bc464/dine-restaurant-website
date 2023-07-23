62a7dfe5c23608de7512d13d9914239fdcdf65ea
bc464 <bclaassen9@gmail.com>
2023-07-23 16:02:39 +0200

    try to deploy to vercel

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/dine-restaurant-website/",
+  __APP_ENV__: process.env.VITE_VERCEL_ENV,
   plugins: [react()],
 })
